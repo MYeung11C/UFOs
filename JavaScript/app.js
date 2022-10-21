b35e6e8db5a3ca705255c043a1d2fa95b8df2151
MYeung11C <106784501+MYeung11C@users.noreply.github.com>
2022-10-21 01:26:17 -0400

    Update app.js

--- JavaScript/app.js
+++ JavaScript/app.js
@@ -75,3 +75,32 @@ function updateFilters() {
   
   // Build the table when the page loads
   buildTable(tableData);
+
+function handleClick() {
+  let date = d3.select("#datetime").property("value");
+  let date = d3.select("#city").property("value");
+  let date = d3.select("#state").property("value");
+  let date = d3.select("#country").property("value");
+  let date = d3.select("#shape").property("value");
+  let filteredData = tableData;
+  
+  // if-statement syntax
+
+  if (datetime) {
+    filteredData = filteredData.filter(row => row.datetime === date);
+};
+  if (city) {
+    filteredData = filteredData.filter(row => row.city === city);
+};
+  if (state) {
+    filteredData = filteredData.filter(row => row.state === state);
+};
+  if (country) {
+    filteredData = filteredData.filter(row => row.country === country);
+};
+  if (shape) {
+    filteredData = filteredData.filter(row => row.shape === shape);
+};
+
+buildTable(filteredData);
+};

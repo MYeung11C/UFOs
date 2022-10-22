a4c77573be1d0fb821e78179075a5843ac938d20
MYeung11C <106784501+MYeung11C@users.noreply.github.com>
2022-10-21 20:20:11 -0400

    Update app.js

--- JavaScript/app.js
+++ JavaScript/app.js
@@ -1,126 +1,100 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-	<meta charset="UTF-8">
-	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title>Document</title>
-</head>
-<body class="bg-dark">
-	<div class=""wrapper">
-		<nav class="navbar navbar-dark bg-dark navbar-expand-lg">
-			<a class="navbar-brand" href="index.html">UFO Sightings</a>
-		</nav>
-		
-		<div class="jumbotron">
-			<var imageUrl="../images/nasa.jpg"></var>
-			<h1 class="display-4">
-				
-					The Truth is Out There
-				
-			</h1>
-			  </div>
-		</div>
-		
-		<div class="container-fluid">
-			<div class="row">
-				<div class="col-md-4">
-					<h3>UFO Sightings: Fact or Fancy? <small>Ufologists Weigh In</small></h3>
-				</div>
-				<div class="col-md-8">
-					<p>
-						Are we alone in the universe? For millennia, humans have turned to the sky to answer this question. Now, thanks to research generously funded by W. Avy, a UFO-enthusiast and amateur ufologist, we can supplement our sky-searching with data analysis.
-						<br><br>
-						"The release of this analysis is well-timed: It coincides with the celebration of World UFO Day, which is a moment for ufologists around the world to connect, relax, and sample a range of UFO-themed snacks," said Dr. Ursula F. Olivier, the world's preeminent expert on circular sightings. "Citizen-scientists can be especially helpful in both cataloguing sightings—which is hugely helpful for us in our search for aliens—and in helping us celebrate the work that has already been done, such as this data visualization project, which will help us raise awareness of the ubiquity of sightings!"
-						<br><br>
-						Not everyone is ready to celebrate, however. Local CEO and vocal anti-alien activist V. Isualize reached out to our reporters to go on record as firmly opposed to any attempts to provide access to this data. "If there are aliens, they certainly would like to be left alone," she stated, before directing us to the Leave Aliens Alone (LAA) community engagement initiative she founded and funds.
-						<br><br>
-						So what do YOU think? Are we alone in the universe? Are aliens trying to contact us, or do they want to be left alone? Dig through the data yourself, and let us know what you see.
-					</p>
-				</div>
-			</div>
-		</div>
-		
-		<div class="container-fluid">
-			<div class="row">
-			  <div class="col-md-3">
-				<form>
-					<p>Filter Search</p>
-					<ul class="list-group">
-						<li class="list-group-item">
-							<label for="date">
-								<label class= "bg-dark">
-									Enter Date
-								</label>
-							</label>
-							<input type="text" placeholder="1/10/2010" id="datetime" />
-						</li>
-						<li class="list-group-item">
-							<label for="City">
-								<label class= "bg-dark">
-									Enter a City
-								</label>
-							</label>
-							<input type="text" placeholder="roswell" id="City" />
-						</li>
-						<li class="list-group-item">
-							<label for="stated">
-								<label class= "bg-dark">
-									Enter a State
-								</label>
-							</label>
-							<input type="text" placeholder="ca" id="atate" />
-						</li>
-						<li class="list-group-item">
-							<label for="country">
-								<label class= "bg-dark">
-									Enter a Country
-								</label>
-							</label>
-							<input type="text" placeholder="us" id="country" />
-						</li>
-						<li class="list-group-item">
-							<label for="Shape">
-								<label class= "bg-dark">
-									Enter a Shape
-								</label>
-							</label>
-							<input type="text" placeholder="circle" id="shape" />
-						</li>
-						<li>
-							<button id="filter-btn" type="button" class="btn btn-default">Filter Table</button>
-						</li>
-					</ul>
-				</form>
-			</div>		
-			<div class="col-md-9">
-				<table class="table table-striped">
-					<thead>
-						<th>Date</th>
-						<th>City</th>
-						<th>State</th>
-						<th>Country</th>
-						<th>Shape</th>
-						<th>Duration</th>
-						<th>Comments</th>
-					</thead>
-					<tbody></tbody>
-				</table>
-			</div>
-		</div>
-	</div>
-</body>
-</html>
+// from data.js
+const tableData = data;
 
-<link
-	rel="stylesheet"
-	href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
-    integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm"
-    crossorigin="anonymous">
-</link>
+// get table references
+var tbody = d3.select("tbody");
 
-<link rel="stylesheet" href="static/css/styles.css"></link>
+function buildTable(data) {
+  // First, clear out any existing data
+  tbody.html("");
 
-<script src="https://cdnjs.cloudflare.com/ajax/libs/d3/4.11.0/d3.js"></script>
-<script src="JavaScript/data.js"></script>
-<script src="JavaScript/app.js"></script>
+  // Next, loop through each object in the data
+  // and append a row and cells for each value in the row
+  data.forEach((dataRow) => {
+    // Append a row to the table body
+    let row = tbody.append("tr");
+
+    // Loop through each field in the dataRow and add
+    // each value as a table cell (td)
+    Object.values(dataRow).forEach((val) => {
+      let cell = row.append("td");
+      cell.text(val);
+    });
+  });
+}
+
+// 1. Create a variable to keep track of all the filters as an object.
+var filter = {};
+
+// 3. Use this function to update the filters. 
+function updateFilter(){
+
+    // 4a. Save the element that was changed as a variable.
+    let date = d3.select("#datetime").property("value");
+    let city = d3.select("#city").property("value");
+    let state = d3.select("#state").property("value");
+    let country = d3.select("#country").property("value");
+    let shape = d3.select("#shape").property("value");
+    let filteredData = tableData;
+  
+    // 4b. Save the value that was changed as a variable.
+  let elementValue = changedElement.property("value");
+  console.log(elementValue);
+
+    // 4c. Save the id of the filter that was changed as a variable.
+  let filterId = changedElement.attr("id");
+  console.log(filterId);
+  
+    // 5. If a filter value was entered then add that filterId and value
+    // to the filters list. Otherwise, clear that filter from the filters object.
+ 
+    if (date) {    
+        filteredData = filteredData.filter(row => row.datetime === date);
+      }
+    if (city) {
+        filteredData = filteredData.filter(row => row.datetime === date);
+      }
+    if (state) {
+        filteredData = filteredData.filter(row => row.datetime === date);
+      }
+    if (country) {
+        filteredData = filteredData.filter(row => row.datetime === date);
+      }
+    if (shape) {
+        filteredData = filteredData.filter(row => row.datetime === date);
+      }
+    else {
+      delete filters[filterId];
+    }
+    
+ 
+  
+    // 6. Call function to apply all filters and rebuild the table
+    filterTable();
+  
+}
+  
+  // 7. Use this function to filter the table when data is entered.
+  function filterTable() {
+  
+    // 8. Set the filtered data to the tableData.
+    var filteredData = tableData;
+  
+    // 9. Loop through all of the filters and keep any data that
+    // matches the filter values
+    Object.entries(filters).forEach(([key, value]) => {
+      filteredData = filteredData.filter(row => row[key] === value);
+    });
+  
+    // 10. Finally, rebuild the table using the filtered data
+     buildTable(filteredData);
+    
+}
+  
+  // 2. Attach an event to listen for changes to each filter
+
+  d3.selectAll("input").on("change", updateFilters);
+  
+  // Build the table when the page loads
+  buildTable(tableData);
+

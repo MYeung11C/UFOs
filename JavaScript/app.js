aa37d1b7901ba6d1283d74759669ccd2b3cf15cf
MYeung11C <106784501+MYeung11C@users.noreply.github.com>
2022-10-21 20:06:32 -0400

    Update app.js

--- JavaScript/app.js
+++ JavaScript/app.js
@@ -73,6 +73,42 @@ function updateFilter(){
 }
   
   // 2. Attach an event to listen for changes to each filter
+  function handleClick() {
+    // Grab the datetime value from the filter
+    let date = d3.select("#datetime").property("value");
+    let city = d3.select("#city").property("value");
+    let state = d3.select("#state").property("value");
+    let country = d3.select("#country").property("value");
+    let shape = d3.select("#shape").property("value");
+    let filteredData = tableData;
+  
+     // Check to see if a date was entered and filter the
+    // data using that date.
+    if (date) {
+      // Apply `filter` to the table data to only keep the
+      // rows where the `datetime` value matches the filter value
+      filteredData = filteredData.filter(row => row.datetime === date);
+    }
+    if (city) {
+        filteredData = filteredData.filter(row => row.datetime === date);
+    }
+    if (state) {
+        filteredData = filteredData.filter(row => row.datetime === date);
+    }
+    if (country) {
+        filteredData = filteredData.filter(row => row.datetime === date);
+    }
+    if (shape) {
+        filteredData = filteredData.filter(row => row.datetime === date);
+    }
+  
+     // Rebuild the table using the filtered data
+    // @NOTE: If no date was entered, then filteredData will
+    // just be the original tableData.
+    buildTable(filteredData);
+  }
+  
+  // Attach an event to listen for the form button
   d3.selectAll("#filter-btn").on("click", handleClick);
   
   // Build the table when the page loads
